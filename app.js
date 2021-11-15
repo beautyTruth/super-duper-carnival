7164d614131f3ef6be3532592a3ca21b6af8bd74
Phil <happy.melodist@gmail.com>
2021-11-14 20:27:46 -0600

    complete coding of dynamic menu and section overlay behaviors in JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+// const hamburgerBtn = document.querySelector(".toggle-btn");
+// const mainHeader = document.querySelector(".main-header");
+// const overlay = document.querySelector(".overlay");
+
+// hamburgerBtn.addEventListener("click", function () {
+//   hamburgerBtn.classList.toggle("open");
+//   overlay.classList.toggle("open");
+//   mainHeader.classList.toggle("open");
+// });
+
+// all that glitters isn't gold
+
 const hamburgerBtn = document.querySelector(".toggle-btn");
 const mainHeader = document.querySelector(".main-header");
 const overlay = document.querySelector(".overlay");
 
-hamburgerBtn.addEventListener("click", function () {
+hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.classList.toggle("open");
   overlay.classList.toggle("open");
   mainHeader.classList.toggle("open");
